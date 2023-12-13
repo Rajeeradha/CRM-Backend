cabb7286377329dde9a941c4d4c1e0a0ca6c605d
Rajeeradha <127470577+Rajeeradha@users.noreply.github.com>
2023-12-13 17:40:45 +1100

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 
 
+mongoose.set("strictQuery", false);
 exports.db = ()=>{
 mongoose.connect(process.env.MONGO_URI).then(()=>{
 console.log('Connection established');
